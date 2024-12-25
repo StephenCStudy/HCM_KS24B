cd2a3a3641e619393e7201e94f325f47caba5d65
StephenCStudy <n24dtcn003@student.ptithcm.edu.vn>
2024-12-25 11:57:40 +0700

    HCM_KS24B_TranDucAnh_01.cpp

--- HCM_KS24B_TranDucAnh_01.cpp
+++ HCM_KS24B_TranDucAnh_01.cpp
@@ -22,8 +22,8 @@ int main() {
         int choice;
         printf("Nhap lua chon cua ban: ");
         scanf("%d", &choice);
-        system("clear");
-        system("cls");
+         system("clear");
+         system("cls");
         
         switch (choice) {
             case 1:
@@ -46,7 +46,7 @@ int main() {
             case 3: {
                 int dem = 0;
                 printf("Cac so nguyen to co trong mang: \n");
-                for(int i = 0; i < n; i++) {
+                 for(int i = 0; i < n; i++) {
                     int num = arr[i];
                     int is_prime = 1;
                     if (num <= 1) {
@@ -60,12 +60,12 @@ int main() {
                         }
                     }
                     if (is_prime) {
-                        printf("- [%d] la so nguyen to\n", arr[i]);
+                        printf("so nguyen to la [%d]\n", arr[i]);
                         dem++;
                     }
                 }
-                printf("So luong so nguyen to: (%d)\n", dem);
-                break;
+               printf("So luong so nguyen to: (%d)\n", dem);
+               break;
             }
                 
             case 4: {
@@ -73,127 +73,123 @@ int main() {
                     printf("Mang phai co tu 3 phan tu tro len!\n");
                 } else {
                     int min = arr[0], min2 = arr[1];
-                    if (min > min2) {
+                     if (min > min2) {
                         int temp = min;
-                        min = min2;
+                         min = min2;
                         min2 = temp;
                     }
                     for (int i = 2; i < n; i++) {
                         if (arr[i] < min) {
-                            min2 = min;
+                             min2 = min;
                             min = arr[i];
-                        } else if (arr[i] < min2 && arr[i] != min) {
+                         } else if (arr[i] < min2 && arr[i] != min) {
                             min2 = arr[i];
                         }
                     }
-                    printf("Gia tri nho thu hai trong mang la: %d\n", min2);
+                     printf("Gia tri nho thu hai trong mang la: %d\n", min2);
                 }
-                break;
+                 break;
             }
                 
             case 5: {
                 int insert_pos, insert_value;
-                printf("Nhap vi tri muon them (1-%d): ", n + 1);
+                 printf("Nhap vi tri muon them (1-%d): ", n + 1);
                 scanf("%d", &insert_pos);
-                if (insert_pos < 1 || insert_pos > n + 1) {
+                 if (insert_pos < 1 || insert_pos > n + 1) {
                     printf("Vi tri khong hop le.\n");
-                    break;
+                     break;
                 }
-                printf("Nhap gia tri muon them: ");
+                 printf("Nhap gia tri muon them: ");
                 scanf("%d", &insert_value);
-                for (int i = n; i >= insert_pos; i--) {
+                 for (int i = n; i >= insert_pos; i--) {
                     arr[i] = arr[i - 1];
                 }
                 arr[insert_pos - 1] = insert_value;
-                n++;                
-                break;
+                 n++;                
+                 break;
             }
                 
             case 6: {
                 int del_pos;
-                printf("Nhap vi tri muon xoa (1-%d): ", n);
+                 printf("Nhap vi tri muon xoa (1-%d): ", n);
                 scanf("%d", &del_pos);
                 if (del_pos < 1 || del_pos > n) {
-                    printf("Vi tri khong hop le.\n");
+                 printf("Vi tri khong hop le.\n");
                     break;
-                }
+              }
                 for (int i = del_pos - 1; i < n - 1; i++) {
-                    arr[i] = arr[i + 1];
+                   arr[i] = arr[i + 1];
                 }
                 n--;                
-                break;
+               break;
             }
                 
             case 7: {                            
-                for(int i=1; i<n; i++){
+               for(int i=1; i<n; i++){
                     int key = arr[i];
-                    int j = i - 1;
+                   int j = i - 1;
                     while(j >= 0 && arr[j] > key){
                         arr[j+1] = arr[j];
                         j = j - 1;
                     }
-                    arr[j+1] = key;
+                   arr[j+1] = key;
                 }
                 printf("Mang sau khi sap xep: \n");
-                for(int i = 0; i < n; i++) {
-                    printf("%d ", arr[i]);
-                }  
+               for(int i = 0; i < n; i++) {
+                   printf("%d ", arr[i]);
+               }  
                 printf("\n");   
-                break;
+               break;
             }      
             case 8: {
-                int search_value;
-                printf("Nhap gia tri muon tim: ");
+               int search_value;
+               printf("Nhap gia tri muon tim: ");
                 scanf("%d", &search_value);
                 int found = 0;
-                for (int i = 0; i < n; i++) {
-                    if (arr[i] == search_value) {
-                        printf("Phan tu %d duoc tim thay tai vi tri %d.\n", search_value, i + 1);
-                        found = 1;
+               for (int i = 0; i < n; i++) {
+                   if (arr[i] == search_value) {
+                       printf("Phan tu [%d] duoc tim thay tai vi tri %d.\n", search_value, i + 1);
+                       found = 1;
                         break;
                     }
                 }
                 if (!found) {
-                    printf("Phan tu %d khong co trong mang.\n", search_value);
+                   printf("Phan tu [%d] khong co trong mang.\n", search_value);
                 }
-                break;
+               break;
             }
                 
             case 9: {
-                for (int i = 0; i < n - 1; i++) {
-                    for (int j = i + 1; j < n; j++) {
-                        if (arr[i] == arr[j]) {
+               for (int i = 0; i < n - 1; i++) {
+                   for (int j = i + 1; j < n; j++) {
+                       if (arr[i] == arr[j]) {
                             for (int k = j; k < n - 1; k++) {
-                                arr[k] = arr[k + 1];
+                               arr[k] = arr[k + 1];
                             }
-                            n--;
-                            j--;
+                           n--;
+                           j--;
                         }
                     }
-                }                
-                printf("Mang sau khi xoa cac phan tu trung lap: \n");
-                for(int i = 0; i < n; i++) {
-                    printf("%d ", arr[i]);
-                }
-                printf("\n");
+               }                                
+               printf("\n");
                 break;
             }
                 
             case 10:
                 for (int i = 0; i < n / 2; i++) {
-                    int temp = arr[i];
+                   int temp = arr[i];
                     arr[i] = arr[n - i - 1];
                     arr[n - i - 1] = temp;
-                }
-                printf("Mang sau khi dao nguoc thu tu: \n");
+               }
+            printf("Mang sau khi dao nguoc thu tu: \n");
                 for (int i = 0; i < n; i++) {
-                    printf("%d ", arr[i]);
+                   printf("%d ", arr[i]);
                 }
-                printf("\n");
+               printf("\n");
                 break;
-                
+               
             case 11:
-                return 0;
+             return 0;
                 
             default:
                 printf("Lua chon khong hop le. Vui long chon lai.\n");
